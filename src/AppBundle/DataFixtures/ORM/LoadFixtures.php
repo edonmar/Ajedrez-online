9a358c3e6bdfb9729db92daba04f312b1f3f5c21
edonmar <edonmar@gmail.com>
2020-06-02 20:00:36 +0200

    Instalados componentes de Fixtures y Alice

---
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Fixtures;
+
+class LoadFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $objects = Fixtures::load(__DIR__.'/fixtures.yml', $manager);
+    }
+}
